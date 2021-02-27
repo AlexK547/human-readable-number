c0dc1e20ab1b5e6afb44df337f1b8e04d604760f
Aleksandr <66899951+AlexK547@users.noreply.github.com>
2021-02-27 23:54:05 +0400

    feat: add tens

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ module.exports = function toReadable (number) {
     let tens = (number / 10) % 10;
     let ones = number % 10;
     let onesArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let tensArr = ['ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
     let result = '';
     
     if (number < 10) {
         result = onesArr[number];
+    } else if (number < 100) {
+        result = `${tensArr[tens]} ${onesArr[ones]}`;
     }
+    
+    if (number === 10) {result = 'ten';}
+    if (number === 100) {result = 'one hundred';}
     
     return result;
 }
