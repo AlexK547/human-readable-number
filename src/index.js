c2e99149fa84e961237609667e635e3c0539f660
Aleksandr <66899951+AlexK547@users.noreply.github.com>
2021-02-27 23:42:30 +0400

    init: start function, add ones

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function toReadable (number) {
-  
+    let hundred = number / 100;
+    let tens = (number / 10) % 10;
+    let ones = number % 10;
+    let onesArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let result = '';
+    
+    if (number < 10) {
+        result = onesArr[number];
+    }
+    
+    return number;
 }
