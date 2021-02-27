12ae4a0275c81847fa100cfac0d2da3a926afec3
Aleksandr <66899951+AlexK547@users.noreply.github.com>
2021-02-27 23:57:55 +0400

    fix: fixed tens

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 module.exports = function toReadable (number) {
     let hundred = number / 100;
-    let tens = (number / 10) % 10;
+    let tens;
     let ones = number % 10;
     let onesArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     let tensArr = ['ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     
     let result = '';
+    
+    if (number < 100 && number > 10) {tens = number / 10;}
     
     if (number < 10) {
         result = onesArr[number];
